f4038001d44530bde970dee05f2da98c7457a85b
unknown <thiagobalves13@gmail.com>
2025-02-19 15:13:28 -0300

    feat(Agendar e Cancelar)[Curso03_Aula01]: Criado métodos para agendar e cancelar consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.ConsultaService;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 
 @RestController
@@ -27,5 +29,14 @@ public class ConsultaController {
 		consultaService.agendar(dados);
 		
 		return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+	}
+	
+	@Transactional
+	@DeleteMapping
+	public ResponseEntity<?> cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados){
+		
+		consultaService.cancelar(dados);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 public record DadosAgendamentoConsulta(Long idMedico, 
 		@NotNull Long idPaciente, 
-		@NotNull @Future LocalDateTime data) {
+		@NotNull @Future LocalDateTime data,
+		Especialidade especialidade) {
 
 }
