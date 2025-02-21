100332e502231ed01e9ec934698d89b869b4d920
unknown <thiagobalves13@gmail.com>
2025-02-21 11:21:33 -0300

    feat(Validador)[Curso03_Aula02]: Criado os validadores

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsulta.java
@@ -0,0 +1,29 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+@Component
+public class ValidadorPacienteSemOutraConsulta implements ValidadorAgendamentoDeConsulta{
+	
+	@Autowired
+	ConsultaRepository repository;
+	
+	public void validar(DadosAgendamentoConsulta dados) {
+		
+		var primeiroHorario = dados.data().withHour(7);
+		var ultimoHorario = dados.data().withHour(18);
+		
+		var pacientePossuiOutraConsulta = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(), 
+				primeiroHorario, ultimoHorario);
+		
+		if(!pacientePossuiOutraConsulta)
+			throw new ValidacaoException("Paciente já possui um consulta agendada nesse dia!");
+		
+	}
+
+}
